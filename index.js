cc821735e915769dde5bcbdf05bae84ba9fa3d63
Ray K <rkcreativetech@gmail.com>
2019-06-10 12:25:05 -0400

    Submission for first deadline

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
@@ -7,12 +7,37 @@ function ask(questionText) {
   });
 }
 
+let loNum = 0;
+let hiNum = 100;
+let yesOrNo = "n";
+let numTries = 0;
+
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
-  process.exit();
+  console.log(
+    "\nPlease think of a number between 1 and 100 (inclusive)\nand I will try to guess it.\n"
+  );
+  let secretNumber = await ask(
+    "What is your secret number? I won't peek, I promise... "
+  );
+  console.log("\nOK, let's begin the game.\n");
+
+  while (yesOrNo === "n") {
+    guess = Math.floor((loNum + hiNum) / 2);
+    numTries = numTries + 1;
+    let yesOrNo = await ask("Is your number " + guess + "? (y/n) ");
+    if (yesOrNo === "y") {
+      console.log("\nYour number was " + guess + "!");
+      console.log("The number of tries I needed was " + numTries + ".\n\n");
+      process.exit();
+    } else {
+      let hiOrLo = await ask("Is your number higher (h) or lower (l)? ");
+      if (hiOrLo === "h") {
+        loNum = guess;
+      } else {
+        hiNum = guess;
+      }
+    }
+  }
 }
